75636919f324483530dd650f0be3f1520b923aca
Harist Islami <islamiharist1998@gmail.com>
2023-01-04 22:40:22 +0700

    first commit

---
+++ src/main/java/com/example/kafkaconsumer/HelloKafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.example.kafkaconsumer;
+
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloKafkaConsumer.class);
+
+    @KafkaListener(topics = "t-hello")
+    public void consume(String message){
+        LOG.info("Consume : {}", message);
+    }
+}
